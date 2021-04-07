2e6f2ed42cc4b511d5e47a08a301fd065932ffdb
nianhuawong <nianhuawong@126.com>
2021-04-07 23:10:16 +0800

    读入边界点坐标作为边界条件

--- Elliptic_Mesh.cpp
+++ Elliptic_Mesh.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
-#include<fstream>
+#include <fstream>
 #include <vector>
 using namespace std;
 #include "Elliptic_Mesh.h"
 
 int main()
 {
+	initialize();
 	read_boundary_points();
+}
+
+void initialize()
+{
+	NI = 202;
+	NJ = 71;
+
+	globalCoordX.resize(NI);
+	for (int i = 0; i < NI; ++i)
+	{
+		globalCoordX[i].resize(NJ);
+	}
+
+	globalCoordY.resize(NI);
+	for (int i = 0; i < NI; ++i)
+	{
+		globalCoordY[i].resize(NJ);
+	}
 }
 
 void read_boundary_points()
 {
-	//read_BC("naca0012/BC1.x");
-	//read_BC("naca0012/BC2.x");
-	read_BC("naca0012/BC3.x");
+	read_BC("naca0012/BC1.x", globalCoordX, globalCoordY, "bottom");
+	read_BC("naca0012/BC2.x", globalCoordX, globalCoordY, "up");
+	read_BC("naca0012/BC3.x", globalCoordX, globalCoordY, "left");
+	read_BC("naca0012/BC3.x", globalCoordX, globalCoordY, "right");
 }
 
-void read_BC(string fileName)
+void read_BC(string fileName, vector < vector<double> >& globalCoordX, vector < vector<double> >& globalCoordY, string pos)
 {
 	fstream file;
 	file.open(fileName, ios_base::in);
@@ -50,5 +70,39 @@ void read_BC(string fileName)
 		}
 		pointCount += numberOfPoints[iZone];
 	}
+
+	if (pos == "bottom")
+	{
+		for (int i = 0; i < NI; ++i)
+		{
+			globalCoordX[i][0] = xCoord[i];
+			globalCoordY[i][0] = yCoord[i];
+		}		
+	}
+	else if (pos == "up")
+	{
+		for (int i = 0; i < NI; ++i)
+		{
+			globalCoordX[i][NJ - 1] = xCoord[i];
+			globalCoordY[i][NJ - 1] = yCoord[i];
+		}
+	}
+	else if (pos == "left")
+	{
+		for (int j = 0; j < NJ; ++j)
+		{
+			globalCoordX[0][j] = xCoord[j];
+			globalCoordY[0][j] = yCoord[j];
+		}
+	}
+	else if (pos == "right")
+	{
+		for (int j = 0; j < NJ; ++j)
+		{
+			globalCoordX[NI-1][j] = xCoord[j];
+			globalCoordY[NI-1][j] = yCoord[j];
+		}
+	}
+
 	int kkk = 1;
 }
--- Elliptic_Mesh.h
+++ Elliptic_Mesh.h
@@ -1,3 +1,8 @@
 #pragma once
+int NI, NJ;
+vector < vector<double> > globalCoordX;
+vector < vector<double> > globalCoordY;
+
 void read_boundary_points();
-void read_BC(string fileName);
+void read_BC(string fileName, vector < vector<double> > &globalCoordX, vector < vector<double> >& globalCoordY, string pos);
+void initialize();
